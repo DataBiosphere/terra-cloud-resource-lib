0bd65fb83d1093dde3769580126fc759bcd4d0ac
yonghao <yyhao1@gmail.com>
2020-05-29 16:18:09 -0400

    javadoc

--- src/main/java/bio/terra/cloudres/common/CowOperation.java
+++ src/main/java/bio/terra/cloudres/common/CowOperation.java
@@ -1,5 +1,16 @@
 package bio.terra.cloudres.common;
 
+/**
+ * interface for a single Cloud Operation
+ *
+ * <p> We expect each Cow implements its own CowOperations includes:
+ * <ul>
+ *     <li> The {@Link CloudOperation}
+ *     <li> how to execute this operation
+ *     <li> how to serialize Request
+ * </ul>
+ *
+ */
 public interface CowOperation<R> {
   CloudOperation getCloudOperation();
 
