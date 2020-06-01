318184c90fea371335877ad66423c8abc896f161
wchambers <1499870+wchamber@users.noreply.github.com>
2020-06-01 15:13:16 -0400

    tweak BucketCowTest

--- src/test/java/bio/terra/cloudres/google/storage/BucketCowTest.java
+++ src/test/java/bio/terra/cloudres/google/storage/BucketCowTest.java
@@ -1,6 +1,5 @@
 package bio.terra.cloudres.google.storage;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.google.cloud.storage.BucketInfo;
@@ -10,11 +9,11 @@ import org.junit.jupiter.api.Test;
 @Tag("integration")
 public class BucketCowTest {
   @Test
-  public void deleteBucket() {
+  public void deleteCreatedBucket() {
     StorageCow storageCow = IntegrationUtils.defaultStorageCow();
     String bucketName = IntegrationUtils.randomName();
     BucketCow bucketCow = storageCow.create(BucketInfo.of(bucketName));
-    assertEquals(bucketCow.getBucketInfo().getName(), bucketName);
+
     assertTrue(bucketCow.delete());
   }
 }
