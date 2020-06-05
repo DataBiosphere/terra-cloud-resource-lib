bf4a82418dfdfbf90419714e1fe106bb89da68de
yonghao <yyhao1@gmail.com>
2020-06-05 12:00:54 -0400

    gradlew spotlessApply

--- src/test/java/bio/terra/cloudres/testing/IntegrationCredentials.java
+++ src/test/java/bio/terra/cloudres/testing/IntegrationCredentials.java
@@ -11,11 +11,12 @@ public class IntegrationCredentials {
 
   /** Path to the admin service account credentials file. */
   private static final String GOOGLE_SERVICE_ACCOUNT_USER_PATH =
-          "src/test/resources/integration_service_account_user.json";
+      "src/test/resources/integration_service_account_user.json";
 
   public static ServiceAccountCredentials getAdminGoogleCredentialsOrDie() {
     try {
-      return ServiceAccountCredentials.fromStream(new FileInputStream(GOOGLE_SERVICE_ACCOUNT_ADMIN_PATH));
+      return ServiceAccountCredentials.fromStream(
+          new FileInputStream(GOOGLE_SERVICE_ACCOUNT_ADMIN_PATH));
     } catch (Exception e) {
       throw new RuntimeException(
           "Unable to load GoogleCredentials from " + GOOGLE_SERVICE_ACCOUNT_ADMIN_PATH + "\n", e);
@@ -24,10 +25,11 @@ public class IntegrationCredentials {
 
   public static ServiceAccountCredentials getRegularUserGoogleCredentialsOrDie() {
     try {
-      return ServiceAccountCredentials.fromStream(new FileInputStream(GOOGLE_SERVICE_ACCOUNT_USER_PATH));
+      return ServiceAccountCredentials.fromStream(
+          new FileInputStream(GOOGLE_SERVICE_ACCOUNT_USER_PATH));
     } catch (Exception e) {
       throw new RuntimeException(
-              "Unable to load GoogleCredentials from " + GOOGLE_SERVICE_ACCOUNT_USER_PATH + "\n", e);
+          "Unable to load GoogleCredentials from " + GOOGLE_SERVICE_ACCOUNT_USER_PATH + "\n", e);
     }
   }
 }
