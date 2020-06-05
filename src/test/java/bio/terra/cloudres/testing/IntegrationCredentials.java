262a8bd104f68894690d9ed120312f916a9420a6
yonghao <yyhao1@gmail.com>
2020-06-05 11:43:52 -0400

    file change

--- src/test/java/bio/terra/cloudres/testing/IntegrationCredentials.java
+++ src/test/java/bio/terra/cloudres/testing/IntegrationCredentials.java
@@ -5,16 +5,29 @@ import java.io.FileInputStream;
 
 /** Provides cloud credentials to use in integration tests. */
 public class IntegrationCredentials {
-  /** Path to the service account credentials file. */
-  private static final String GOOGLE_SERVICE_ACCOUNT_PATH =
-      "src/test/resources/integration_service_account.json";
+  /** Path to the admin service account credentials file. */
+  private static final String GOOGLE_SERVICE_ACCOUNT_ADMIN_PATH =
+      "src/test/resources/integration_service_account_admin.json";
 
-  public static ServiceAccountCredentials getGoogleCredentialsOrDie() {
+  /** Path to the admin service account credentials file. */
+  private static final String GOOGLE_SERVICE_ACCOUNT_USER_PATH =
+          "src/test/resources/integration_service_account_user.json";
+
+  public static ServiceAccountCredentials getAdminGoogleCredentialsOrDie() {
     try {
-      return ServiceAccountCredentials.fromStream(new FileInputStream(GOOGLE_SERVICE_ACCOUNT_PATH));
+      return ServiceAccountCredentials.fromStream(new FileInputStream(GOOGLE_SERVICE_ACCOUNT_ADMIN_PATH));
     } catch (Exception e) {
       throw new RuntimeException(
-          "Unable to load GoogleCredentials from " + GOOGLE_SERVICE_ACCOUNT_PATH + "\n", e);
+          "Unable to load GoogleCredentials from " + GOOGLE_SERVICE_ACCOUNT_ADMIN_PATH + "\n", e);
+    }
+  }
+
+  public static ServiceAccountCredentials getRegularUserGoogleCredentialsOrDie() {
+    try {
+      return ServiceAccountCredentials.fromStream(new FileInputStream(GOOGLE_SERVICE_ACCOUNT_USER_PATH));
+    } catch (Exception e) {
+      throw new RuntimeException(
+              "Unable to load GoogleCredentials from " + GOOGLE_SERVICE_ACCOUNT_USER_PATH + "\n", e);
     }
   }
 }
--- src/test/java/bio/terra/cloudres/testing/IntegrationCredentialsTest.java
+++ src/test/java/bio/terra/cloudres/testing/IntegrationCredentialsTest.java
@@ -8,9 +8,18 @@ import org.junit.jupiter.api.Test;
 @Tag("integration")
 public class IntegrationCredentialsTest {
   @Test
-  public void googleCredentialsAreAccessible() {
+  public void googleCredentialsAreAccessible_adminUser() {
     try {
-      IntegrationCredentials.getGoogleCredentialsOrDie();
+      IntegrationCredentials.getAdminGoogleCredentialsOrDie();
+    } catch (Exception e) {
+      fail("Should not have thrown any exception getting credentials, but threw " + e);
+    }
+  }
+
+  @Test
+  public void googleCredentialsAreAccessible_regularUser() {
+    try {
+      IntegrationCredentials.getRegularUserGoogleCredentialsOrDie();
     } catch (Exception e) {
       fail("Should not have thrown any exception getting credentials, but threw " + e);
     }
