4e4cedcde0afd7e01a2a61ec360f8d55b0e27883
yonghao <yyhao1@gmail.com>
2020-06-12 11:48:35 -0400

    user json string in test

--- src/test/java/bio/terra/cloudres/google/bigquery/SerializeUtilsTest.java
+++ src/test/java/bio/terra/cloudres/google/bigquery/SerializeUtilsTest.java
@@ -2,7 +2,6 @@ package bio.terra.cloudres.google.bigquery;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import bio.terra.cloudres.testing.IntegrationUtils;
 import com.google.cloud.bigquery.*;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -10,18 +9,14 @@ import org.junit.jupiter.api.Test;
 @Tag("unit")
 public class SerializeUtilsTest {
   private final DatasetId datasetId = DatasetId.of("datasetId1");
-  private final String jsonDatasetId = "{\"datasetId\":{\"dataset\":\"datasetId1\"}";
   private final TableId tableId = TableId.of(datasetId.getDataset(), "tableId1");
   private final String jsonTableId =
       "{\"tableId\":{\"dataset\":\"datasetId1\",\"table\":\"tableId1\"}";
 
-  private static final String REUSABLE_DATASET_ID = IntegrationUtils.randomNameWithUnderscore();
-
   @Test
   public void convertDatasetIdWithOptions() {
     assertEquals(
-        jsonDatasetId
-            + ",\"datasetOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,access\"},{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,creationTime\"}]}",
+        "{\"datasetId\":{\"dataset\":\"datasetId1\"},\"datasetOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,access\"},{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,creationTime\"}]}",
         SerializeUtils.convert(
                 datasetId,
                 BigQuery.DatasetOption.fields(BigQuery.DatasetField.ACCESS),
@@ -32,9 +27,7 @@ public class SerializeUtilsTest {
   @Test
   public void convertDatasetInfoWithOptions() {
     assertEquals(
-        "{\"datasetInfo\":"
-            + jsonDatasetId
-            + ",\"labels\":{\"userMap\":{}}},\"datasetOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,access\"},{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,creationTime\"}]}",
+        "{\"datasetInfo\":{\"datasetId\":{\"dataset\":\"datasetId1\"},\"labels\":{\"userMap\":{}}},\"datasetOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,access\"},{\"rpcOption\":\"FIELDS\",\"value\":\"datasetReference,creationTime\"}]}",
         SerializeUtils.convert(
                 DatasetInfo.newBuilder(datasetId).build(),
                 BigQuery.DatasetOption.fields(BigQuery.DatasetField.ACCESS),
@@ -45,8 +38,7 @@ public class SerializeUtilsTest {
   @Test
   public void convertDatasetInfoWithDeleteOptions() {
     assertEquals(
-        jsonDatasetId
-            + ",\"datasetDeleteOptions\":[{\"rpcOption\":\"DELETE_CONTENTS\",\"value\":true}]}",
+        "{\"datasetId\":{\"dataset\":\"datasetId1\"},\"datasetDeleteOptions\":[{\"rpcOption\":\"DELETE_CONTENTS\",\"value\":true}]}",
         SerializeUtils.convert(datasetId, BigQuery.DatasetDeleteOption.deleteContents())
             .toString());
   }
@@ -54,9 +46,7 @@ public class SerializeUtilsTest {
   @Test
   public void convertTableInfoWithTableOptions() {
     assertEquals(
-        "{\"tableInfo\":"
-            + jsonTableId
-            + ",\"definition\":{\"type\":{\"constant\":\"TABLE\"},\"location\":\"location\"},\"labels\":{\"userMap\":{}}},\"tableOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"type,tableReference\"}]}",
+        "{\"tableInfo\":{\"tableId\":{\"dataset\":\"datasetId1\",\"table\":\"tableId1\"},\"definition\":{\"type\":{\"constant\":\"TABLE\"},\"location\":\"location\"},\"labels\":{\"userMap\":{}}},\"tableOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"type,tableReference\"}]}",
         SerializeUtils.convert(
                 TableInfo.newBuilder(
                         tableId,
@@ -68,29 +58,29 @@ public class SerializeUtilsTest {
 
   @Test
   public void convertTableId() {
-    assertEquals(jsonTableId + "}", SerializeUtils.convert(tableId).toString());
+    assertEquals(
+        "{\"tableId\":{\"dataset\":\"datasetId1\",\"table\":\"tableId1\"}}",
+        SerializeUtils.convert(tableId).toString());
   }
 
   @Test
   public void convertTableIdWithTableOptions() {
     assertEquals(
-        jsonTableId
-            + ",\"tableOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"type,tableReference\"}]}",
+        "{\"tableId\":{\"dataset\":\"datasetId1\",\"table\":\"tableId1\"},\"tableOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"type,tableReference\"}]}",
         SerializeUtils.convert(tableId, BigQuery.TableOption.fields()).toString());
   }
 
   @Test
   public void convertDatasetIdWithTableOptions() {
     assertEquals(
-        jsonDatasetId + ",\"tableListOptions\":[{\"rpcOption\":\"MAX_RESULTS\",\"value\":1}]}",
+        "{\"datasetId\":{\"dataset\":\"datasetId1\"},\"tableListOptions\":[{\"rpcOption\":\"MAX_RESULTS\",\"value\":1}]}",
         SerializeUtils.convert(datasetId, BigQuery.TableListOption.pageSize(1)).toString());
   }
 
   @Test
   public void convertTableIdWithTableDefinitionWithTableOption() {
     assertEquals(
-        jsonTableId
-            + ",\"tableDefinition\":{\"type\":{\"constant\":\"TABLE\"},\"location\":\"location\"},\"tableOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"type,tableReference\"}]}",
+        "{\"tableId\":{\"dataset\":\"datasetId1\",\"table\":\"tableId1\"},\"tableDefinition\":{\"type\":{\"constant\":\"TABLE\"},\"location\":\"location\"},\"tableOptions\":[{\"rpcOption\":\"FIELDS\",\"value\":\"type,tableReference\"}]}",
         SerializeUtils.convert(
                 tableId,
                 StandardTableDefinition.newBuilder().setLocation("location").build(),
