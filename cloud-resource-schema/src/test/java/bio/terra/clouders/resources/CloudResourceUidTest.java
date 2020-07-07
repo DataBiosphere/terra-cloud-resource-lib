6e3c5ff3d9ecc6e1272b9762dba9ed7b6935a50d
yonghao <yyhao1@gmail.com>
2020-07-07 09:59:36 -0400

    move cloud resource uid test to right directory

--- cloud-resource-schema/src/test/java/bio/terra/clouders/resources/CloudResourceUidTest.java
+++ cloud-resource-schema/src/test/java/bio/terra/clouders/resources/CloudResourceUidTest.java
@@ -1,13 +1,14 @@
-package bio.terra.cloudres.resources;
+package bio.terra.clouders.resources;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
-
+import bio.terra.cloudres.resources.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /** Tests to verify properties of the CloudResourceUid generated models. */
 @Tag("unit")
@@ -18,7 +19,7 @@ public class CloudResourceUidTest {
    * Asserts that serializing->deserializing a {@link CloudResourceUid} yields an "equals" instance.
    */
   public void assertSerializationIdempotency(
-      CloudResourceUid resource, java.lang.Class<?> resourceClass) throws JsonProcessingException {
+      CloudResourceUid resource, Class<?> resourceClass) throws JsonProcessingException {
     String serialized = objectMapper.writeValueAsString(resource);
     CloudResourceUid deserialized = objectMapper.readValue(serialized, resource.getClass());
     // Asserts that serializing->deserializing a {@link CloudResourceUid} yields an "equals"
