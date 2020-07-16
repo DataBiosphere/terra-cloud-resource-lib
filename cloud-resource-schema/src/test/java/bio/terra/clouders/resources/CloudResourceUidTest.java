54ad861b59c43d394c3ce7ad00d49baf3d255ac5
yonghao <yyhao1@gmail.com>
2020-07-16 12:20:48 -0400

    address comment

--- cloud-resource-schema/src/test/java/bio/terra/clouders/resources/CloudResourceUidTest.java
+++ cloud-resource-schema/src/test/java/bio/terra/clouders/resources/CloudResourceUidTest.java
@@ -1,12 +1,10 @@
 package bio.terra.clouders.resources;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import bio.terra.cloudres.resources.*;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -18,15 +16,14 @@ public class CloudResourceUidTest {
   /**
    * Asserts that serializing->deserializing a {@link CloudResourceUid} yields an "equals" instance.
    */
-  public void assertSerializationIdempotency(CloudResourceUid resource, Class<?> resourceClass)
-      throws JsonProcessingException {
+  public void assertSerializationIdempotency(CloudResourceUid resource, String getMethodName)
+      throws Exception {
     String serialized = objectMapper.writeValueAsString(resource);
     CloudResourceUid deserialized = objectMapper.readValue(serialized, resource.getClass());
     // Asserts that serializing->deserializing a {@link CloudResourceUid} yields an "equals"
     // instance.
     assertEquals(resource, deserialized);
-    // Assert that the deserialized instance is an instance of the same class.
-    assertThat(resource, Matchers.instanceOf(resourceClass));
+    assertNotNull(CloudResourceUid.class.getMethod(getMethodName));
   }
 
   @Test
@@ -36,6 +33,40 @@ public class CloudResourceUidTest {
             .googleBigQueryDatasetUid(
                 new GoogleBigQueryDatasetUid().projectId("my-project").datasetId("my-dataset"));
 
-    assertSerializationIdempotency(dataset, CloudResourceUid.class);
+    assertSerializationIdempotency(dataset, "getGoogleBigQueryDatasetUid");
+  }
+
+  @Test
+  public void googleBigQueryTable() throws Exception {
+    CloudResourceUid table =
+        new CloudResourceUid()
+            .googleBigQueryTableUid(
+                new GoogleBigQueryTableUid()
+                    .projectId("my-project")
+                    .datasetId("my-dataset")
+                    .tableId("my-table"));
+    assertSerializationIdempotency(table, "getGoogleBigQueryTableUid");
+  }
+
+  @Test
+  public void googleBlob() throws Exception {
+    CloudResourceUid blob =
+        new CloudResourceUid()
+            .googleBlobUid(new GoogleBlobUid().bucketName("my-bucket").blobName("my-blob"));
+    assertSerializationIdempotency(blob, "getGoogleBlobUid");
+  }
+
+  @Test
+  public void googleBucket() throws Exception {
+    CloudResourceUid bucket =
+        new CloudResourceUid().googleBucketUid(new GoogleBucketUid().bucketName("my-bucket"));
+    assertSerializationIdempotency(bucket, "getGoogleBucketUid");
+  }
+
+  @Test
+  public void googleProject() throws Exception {
+    CloudResourceUid project =
+        new CloudResourceUid().googleProjectUid(new GoogleProjectUid().projectId("my-project"));
+    assertSerializationIdempotency(project, "getGoogleProjectUid");
   }
 }
