93519f1e4d792efca0e1bf63161b75b641fe04af
jdcanas <jcanas@broadinstitute.com>
2021-07-28 14:26:14 -0700

    initial code added

---
+++ azure-resource-manager/src/test/java/bio/terra/cloudres/azure/resource/manager/AzureResourceTest.java
@@ -0,0 +1,14 @@
+package bio.terra.cloudres.azure.resource.manager;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+// @Tag("integration")
+public class AzureResourceTest {
+
+  @Test
+  public void assertTest() {
+    assertTrue(true);
+  }
+}
