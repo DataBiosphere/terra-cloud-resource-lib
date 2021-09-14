7ea298a91f312b8c0f8146fb0e907d33ccfae448
Justin Canas <justincanas16@gmail.com>
2021-09-14 12:39:14 -0400

    [IA-2860] Azure models/tests for Network / Disk / VM resources CREATE / GET / DELETE (#99)
    * tests passing for vm creation with resources
    
    * refactor model
    
    * cleanup
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- azure-resourcemanager-compute/src/main/java/bio/terra/cloudres/azure/resourcemanager/compute/CreatePublicIpRequestData.java
+++ azure-resourcemanager-compute/src/main/java/bio/terra/cloudres/azure/resourcemanager/compute/CreatePublicIpRequestData.java
@@ -1,28 +1,18 @@
 package bio.terra.cloudres.azure.resourcemanager.compute;
 
-import bio.terra.cloudres.azure.resourcemanager.resources.AbstractRequestData;
+import bio.terra.cloudres.azure.resourcemanager.resources.AzureRequestData;
 import com.azure.core.management.Region;
 import com.google.gson.JsonObject;
 
 /** Data for an Azure IP creation request. */
-public class CreatePublicIpRequestData extends AbstractRequestData {
-  private final String resourceGroupName;
-  private final String name;
-  private final Region region;
+public class CreatePublicIpRequestData extends BaseRequestData {
 
-  public CreatePublicIpRequestData(String resourceGroupName, String name, Region region) {
-    super(ComputeManagerOperation.AZURE_CREATE_PUBLIC_IP);
-    this.resourceGroupName = resourceGroupName;
-    this.name = name;
-    this.region = region;
+  protected PublicIpRequestData(String resourceGroupName, String name, Region region) {
+    super(ComputeManagerOperation.AZURE_CREATE_PUBLIC_IP, resourceGroupName, name, region);
   }
 
   @Override
-  public JsonObject serialize() {
-    JsonObject requestData = new JsonObject();
-    requestData.addProperty("resourceGroupName", resourceGroupName);
-    requestData.addProperty("name", name);
-    requestData.addProperty("region", region.name());
-    return requestData;
+  public JsonObject getRequestData() {
+    return super.getRequestData();
   }
 }
