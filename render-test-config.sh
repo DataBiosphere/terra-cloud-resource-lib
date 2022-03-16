b4ab7081f4d7d73351b4cbccf3a5c82f02b89c80
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2022-03-16 16:18:15 -0400

    fix integration tests

--- render-test-config.sh
+++ render-test-config.sh
@@ -10,11 +10,11 @@ VAULT_SERVICE_ACCOUNT_ADMIN_PATH=secret/dsde/terra/crl-test/default/service-acco
 VAULT_SERVICE_ACCOUNT_USER_PATH=secret/dsde/terra/crl-test/default/service-account-user.json
 VAULT_SERVICE_ACCOUNT_JANITOR_CLIENT_PATH=secret/dsde/terra/kernel/integration/tools/crl_janitor/client-sa
 VAULT_AZURE_MANAGED_APP_CLIENT_PATH=secret/dsde/terra/azure/common/managed-app-publisher
-SERVICE_ACCOUNT_ADMIN_OUTPUT_FILE_PATH="$(dirname $0)"/common/src/testFixtures/resources/integration_service_account_admin.json
-SERVICE_ACCOUNT_USER_OUTPUT_FILE_PATH="$(dirname $0)"/common/src/testFixtures/resources/integration_service_account_user.json
-SERVICE_ACCOUNT_JANITOR_CLIENT_OUTPUT_FILE_PATH="$(dirname $0)"/common/src/testFixtures/resources/integration_service_account_janitor_client.json
-AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH="$(dirname $0)"/azure-resourcemanager-common/src/testFixtures/resources/integration_azure_managed_app_client.json
-AZURE_PROPERTIES_OUTPUT_FILE_PATH="$(dirname $0)"/azure-resourcemanager-common/src/testFixtures/resources/integration_azure_env.properties
+SERVICE_ACCOUNT_ADMIN_OUTPUT_FILE_PATH="$(dirname $0)"/src/testFixtures/resources/integration_service_account_admin.json
+SERVICE_ACCOUNT_USER_OUTPUT_FILE_PATH="$(dirname $0)"/src/testFixtures/resources/integration_service_account_user.json
+SERVICE_ACCOUNT_JANITOR_CLIENT_OUTPUT_FILE_PATH="$(dirname $0)"/src/testFixtures/resources/integration_service_account_janitor_client.json
+AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH="$(dirname $0)"/src/testFixtures/resources/integration_azure_managed_app_client.json
+AZURE_PROPERTIES_OUTPUT_FILE_PATH="$(dirname $0)"/src/testFixtures/resources/integration_azure_env.properties
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOLBOX_DOCKER_IMAGE} \
             vault read -format json ${VAULT_SERVICE_ACCOUNT_ADMIN_PATH} \
