71df3f243f77128760f8c7cf7011335548f874b1
Robert Title <rtitle@broadinstitute.org>
2022-06-13 22:58:49 -0400

    Add janitor support; bump azure deps; spotless

--- src/main/java/bio/terra/cloudres/azure/resourcemanager/msi/MsiManagerOperation.java
+++ src/main/java/bio/terra/cloudres/azure/resourcemanager/msi/MsiManagerOperation.java
@@ -4,5 +4,5 @@ import bio.terra.cloudres.common.CloudOperation;
 
 /** {@link CloudOperation} for using Azure MsiManager API. */
 public enum MsiManagerOperation implements CloudOperation {
-    CREATE_USER_ASSIGNED_MANAGED_IDENTITY
+  CREATE_USER_ASSIGNED_MANAGED_IDENTITY
 }
