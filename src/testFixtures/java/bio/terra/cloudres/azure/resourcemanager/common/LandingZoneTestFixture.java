8cfc2e1d6a6d63f7547994a412c6b1b4435dd6b3
giventocode <3589801+giventocode@users.noreply.github.com>
2022-07-12 15:23:51 -0400

    java 17 GHA, and build

--- src/testFixtures/java/bio/terra/cloudres/azure/resourcemanager/common/LandingZoneTestFixture.java
+++ src/testFixtures/java/bio/terra/cloudres/azure/resourcemanager/common/LandingZoneTestFixture.java
@@ -1,17 +1,11 @@
 package bio.terra.cloudres.azure.resourcemanager.common;
 
 import bio.terra.cloudres.azure.landingzones.definition.ArmManagers;
-import bio.terra.cloudres.azure.landingzones.deployment.DeployedResource;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 /** wires up ARM clients and creates a test resource group. */
 public class LandingZoneTestFixture {
@@ -36,16 +30,5 @@ public class LandingZoneTestFixture {
   @AfterAll
   static void cleanUpAfterAll() {
     armManagers.azureResourceManager().resourceGroups().deleteByName(resourceGroup.name());
-  }
-
-  protected void assertAllResourcesExists(List<DeployedResource> resources) {
-    for (DeployedResource resource : resources) {
-      assertThat(
-          armManagers
-              .azureResourceManager()
-              .genericResources()
-              .checkExistenceById(resource.resourceId()),
-          equalTo(true));
-    }
   }
 }
