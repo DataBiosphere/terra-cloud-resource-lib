e186d1670d4e69151e9ed27d040bb1946cdec1f6
Jordan Safer <jordansafer2@gmail.com>
2022-07-28 11:20:41 -0700

    starting new resources for cromwell

---
+++ src/main/java/bio/terra/cloudres/azure/landingzones/definition/factories/CromwellBaseResourcesFactory.java
@@ -0,0 +1,125 @@
+package bio.terra.cloudres.azure.landingzones.definition.factories;
+
+import bio.terra.cloudres.azure.landingzones.definition.*;
+import bio.terra.cloudres.azure.landingzones.deployment.LandingZoneDeployment.DefinitionStages.Deployable;
+import bio.terra.cloudres.azure.landingzones.deployment.LandingZoneDeployment.DefinitionStages.WithLandingZoneResource;
+import bio.terra.cloudres.azure.landingzones.deployment.ResourcePurpose;
+import bio.terra.cloudres.azure.landingzones.deployment.SubnetResourcePurpose;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.containerservice.models.AgentPoolMode;
+import com.azure.resourcemanager.containerservice.models.ContainerServiceVMSizeTypes;
+import com.azure.resourcemanager.relay.RelayManager;
+import com.azure.resourcemanager.resources.models.ResourceGroup;
+import java.util.List;
+
+/**
+ * An implementation of {@link LandingZoneDefinitionFactory} that deploys resources required for
+ * cromwell. Current resources are: - VNet: Subnets required for AKS, Batch, PostgreSQL and
+ * Compute/VMs - AKS Account (?) TODO - AKS Nodepool TODO - Batch Account TODO - Storage Account
+ * TODO - PostgreSQL server TODO
+ */
+public class CromwellBaseResourcesFactory extends ArmClientsDefinitionFactory {
+  private final String LZ_NAME = "Cromwell Landing Zone Base Resources";
+  private final String LZ_DESC =
+      "Cromwell Base Resources: VNet, AKS Account & Nodepool, Batch Account,"
+          + " Storage Account, PostgreSQL server, Subnets for AKS, Batch, Posgres, and Compute";
+
+  public CromwellBaseResourcesFactory(
+      AzureResourceManager azureResourceManager, RelayManager relayManager) {
+    super(azureResourceManager, relayManager);
+  }
+
+  @Override
+  public DefinitionHeader header() {
+    return new DefinitionHeader(LZ_NAME, LZ_DESC);
+  }
+
+  @Override
+  public List<DefinitionVersion> availableVersions() {
+    return List.of(DefinitionVersion.V1);
+  }
+
+  @Override
+  public LandingZoneDefinable create(DefinitionVersion version) {
+    if (version.equals(DefinitionVersion.V1)) {
+      return new DefinitionV1(azureResourceManager, relayManager);
+    }
+    throw new RuntimeException("Invalid Version");
+  }
+
+  class DefinitionV1 extends LandingZoneDefinition {
+
+    protected DefinitionV1(AzureResourceManager azureResourceManager, RelayManager relayManager) {
+      super(azureResourceManager, relayManager);
+    }
+
+    @Override
+    public Deployable definition(DefinitionContext definitionContext) {
+      WithLandingZoneResource deployment = definitionContext.deployment();
+      ResourceGroup resourceGroup = definitionContext.resourceGroup();
+      ResourceNameGenerator nameGenerator = definitionContext.resourceNameGenerator();
+
+      var vNet =
+          azureResourceManager
+              .networks()
+              .define(nameGenerator.nextName(ResourceNameGenerator.MAX_VNET_NAME_LENGTH))
+              .withRegion(resourceGroup.region())
+              .withExistingResourceGroup(resourceGroup)
+              .withAddressSpace("10.0.0.0/28")
+              .withSubnet("aks", "10.0.0.0/29")
+              .withSubnet("batch", "10.0.0.0/30")
+              .withSubnet("postgres", "10.0.0.0/31")
+              .withSubnet("compute", "10.0.0.0/32");
+
+      var aks =
+          azureResourceManager
+              .kubernetesClusters()
+              .define(nameGenerator.nextName(ResourceNameGenerator.MAX_AKS_CLUSTER_NAME_LENGTH))
+              .withRegion(resourceGroup.region())
+              .withExistingResourceGroup(resourceGroup)
+              .withDefaultVersion()
+              .withSystemAssignedManagedServiceIdentity()
+              .defineAgentPool(
+                  nameGenerator.nextName(ResourceNameGenerator.MAX_AKS_AGENT_POOL_NAME_LENGTH))
+              .withVirtualMachineSize(ContainerServiceVMSizeTypes.STANDARD_A2_V2)
+              .withAgentPoolVirtualMachineCount(1)
+              .withAgentPoolMode(
+                  AgentPoolMode.SYSTEM) // TODO VM Size? Pool Machine count? AgentPoolMode?
+              .attach()
+              .withDnsPrefix(
+                  nameGenerator.nextName(ResourceNameGenerator.MAX_AKS_DNS_PREFIX_NAME_LENGTH));
+
+      //   TODO can we make this async/sequential after deployment?
+      //      var stopAks = azureResourceManager
+      //              .kubernetesClusters()
+      //              .stop(resourceGroup.name(), aks.name());
+
+      //      var batch =
+      //              azureResourceManager
+
+      var storage =
+          azureResourceManager
+              .storageAccounts()
+              .define(nameGenerator.nextName(ResourceNameGenerator.MAX_STORAGE_ACCOUNT_NAME_LENGTH))
+              .withRegion(resourceGroup.region())
+              .withExistingResourceGroup(resourceGroup);
+
+      /*   TODO do we need this?
+      var relay =
+       relayManager
+           .namespaces()
+           .define(nameGenerator.nextName(ResourceNameGenerator.MAX_RELAY_NS_NAME_LENGTH))
+           .withRegion(resourceGroup.region())
+           .withExistingResourceGroup(resourceGroup.name());*/
+
+      return deployment
+          .withVNetWithPurpose(vNet, "aks", SubnetResourcePurpose.WORKSPACE_COMPUTE_SUBNET)
+          .withVNetWithPurpose(vNet, "batch", SubnetResourcePurpose.WORKSPACE_COMPUTE_SUBNET)
+          .withVNetWithPurpose(vNet, "postgres", SubnetResourcePurpose.WORKSPACE_COMPUTE_SUBNET)
+          .withVNetWithPurpose(vNet, "compute", SubnetResourcePurpose.WORKSPACE_COMPUTE_SUBNET)
+          .withResourceWithPurpose(aks, ResourcePurpose.SHARED_RESOURCE)
+          // .withResourceWithPurpose(relay, ResourcePurpose.SHARED_RESOURCE)
+          .withResourceWithPurpose(storage, ResourcePurpose.SHARED_RESOURCE);
+    }
+  }
+}
