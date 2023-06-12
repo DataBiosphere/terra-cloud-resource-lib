7e4a455b102623071f981716327cb9e17fa6aff3
Marcus Talbott <mtalbott@broadinstitute.org>
2023-06-12 14:44:00 -0400

    janitor storage container support

---
+++ src/main/java/bio/terra/cloudres/azure/resourcemanager/storage/StorageManagerOperation.java
@@ -0,0 +1,8 @@
+package bio.terra.cloudres.azure.resourcemanager.storage;
+
+import bio.terra.cloudres.common.CloudOperation;
+
+/** {@link CloudOperation} for using Azure StorageManager API. */
+public enum StorageManagerOperation implements CloudOperation {
+  AZURE_CREATE_STORAGE_CONTAINER
+}
