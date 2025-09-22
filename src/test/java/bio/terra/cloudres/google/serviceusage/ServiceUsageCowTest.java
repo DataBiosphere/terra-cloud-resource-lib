091b6724be8da526b033dff593c2dc5cc44030e0
David An <davidan@broadinstitute.org>
2025-09-22 14:11:12 -0400

    spotless

--- src/test/java/bio/terra/cloudres/google/serviceusage/ServiceUsageCowTest.java
+++ src/test/java/bio/terra/cloudres/google/serviceusage/ServiceUsageCowTest.java
@@ -30,7 +30,6 @@ public class ServiceUsageCowTest {
 
   public static final long OVERRIDE_VALUE_BYTES = 209_715_199L; // previously 1_099_511_627_776L;
 
-
   private static final String STORAGE_SERVICE_ID = "storage-api.googleapis.com";
   private static final String ENABLED_FILTER = "state:ENABLED";
   public static final String QUOTA_METRIC = "bigquery.googleapis.com/quota/query/usage";
